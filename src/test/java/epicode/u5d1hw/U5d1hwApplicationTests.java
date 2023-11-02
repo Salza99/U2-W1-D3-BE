87863d137eddff127e3dc997848974f7170537ff
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-11-02 15:18:30 +0100

    initialize project from solution D2

---
+++ src/test/java/epicode/u5d1hw/U5d1hwApplicationTests.java
@@ -0,0 +1,18 @@
+package epicode.u5d1hw;
+
+import epicode.u5d1hw.entities.pizzas.Pizza;
+import epicode.u5d1hw.entities.pizzas.PizzaMargherita;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+
+@SpringBootTest
+class U5d1hwApplicationTests {
+
+
+}
